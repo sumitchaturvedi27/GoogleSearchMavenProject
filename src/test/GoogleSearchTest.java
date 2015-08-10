15a6e31b22b6e3dc88ec873dca57d64249eb2f3d
Sumit <cool.sumit.chaturvedi@gmail.com>
2015-08-10 17:19:24 +0530

    testlink-api-client-2.0.jar added

--- src/test/GoogleSearchTest.java
+++ src/test/GoogleSearchTest.java
@@ -1,35 +1,35 @@
 package test;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-//import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClient;
 //import org.apache.xmlrpc.*;
-//import org.apache.ws.commons.serialize.DOMSerializer;
-//import testlink.api.java.client.TestLinkAPIClient;
-//import testlink.api.java.client.TestLinkAPIException;
-//import testlink.api.java.client.TestLinkAPIResults;
+import org.apache.ws.commons.serialize.DOMSerializer;
+
+import testlink.api.java.client.TestLinkAPIClient;
+import testlink.api.java.client.TestLinkAPIException;
+import testlink.api.java.client.TestLinkAPIResults;
+
 
 public class GoogleSearchTest {
   
-	public static String DEVKEY="0919fe8239ba5be746ae7a1d44f50a7f9a1e6dff7dbc2da64a3577c8b9a24854";
+	public static String DEVKEY="fcb75a5d649ed079300b46195d2616ac";
 	
 	public static String URL = "http://localhost//testlink//lib//api//xmlrpc//v1//xmlrpc.php";
 	
-	/*public void reportResult(String testProject, String testPlan, String testCase, String Build, String Notes, String Result) throws TestLinkAPIException {
+	public void reportResult(String testProject, String testPlan, String testCase, String Build, String Notes, String Result) throws TestLinkAPIException {
 		
 		TestLinkAPIClient api = new TestLinkAPIClient (DEVKEY, URL);
 		
 		api.reportTestCaseResult(testProject, testPlan, testCase, Build, Notes, Result);
 		
-	} */
+	}
   @Test
-  public void searchGoogle ()  {
-	  
-	 
+  public void searchGoogle () throws TestLinkAPIException  {
 	  
 	  System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver.exe");
 	  
@@ -57,14 +57,14 @@ public class GoogleSearchTest {
 	  
 	  
 	  
-	  //Result = TestLinkAPIResults.TEST_PASSED;
+	  Result = TestLinkAPIResults.TEST_PASSED;
 	  Notes = "Executed Successfully";
 	  } catch (Exception e) {
-		  //Result = TestLinkAPIResults.TEST_FAILED;
+		  Result = TestLinkAPIResults.TEST_FAILED;
 		  Notes = "Executed Failed";
 	  }
 	  finally {
-		//  t.reportResult(testProject, testPlan, testCase, Build, Notes, Result);
+		  t.reportResult(testProject, testPlan, testCase, Build, Notes, Result);
 		  driver.quit();
 	  }
   }
